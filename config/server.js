23877d229c15c8819d58229dc0c0cfd2db88ce55
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-07-18 16:32:31 +0900

    modified config

--- config/server.js
+++ config/server.js
@@ -1,33 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1342),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
-  middleware: {
-    session: {
-      enabled: true,
-      keys: env.array('APP_KEYS'),
-    },
-  },
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'event'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', '0795'),
-      schema: env('DATABASE_SCHEMA', 'public'), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      },
-    },
-    debug: false,
+    keys: env.array('APP_KEYS')
   },
 });
-
-// i
