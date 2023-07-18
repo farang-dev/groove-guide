62de056710643d23f26cb690543497c600010803
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-07-19 02:54:57 +0900

    modified config

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => {
   const connections = {
     postgres: {
       connection: {
-        host: env('DATABASE_HOST', '0.0.0.0'),
+        host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'event'),
         user: env('DATABASE_USERNAME', 'postgres'),
