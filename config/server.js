494025aedebc169eb8ff0b469e13064fb4890410
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-07-20 02:16:13 +0900

    modified config

--- config/server.js
+++ config/server.js
@@ -1,3 +1,13 @@
 module.exports = ({ env }) => ({
   url: env("RENDER_EXTERNAL_URL"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
+    },
+  },
+  app: {
+    keys: [env('APP_KEYS')],
+  },
 });
